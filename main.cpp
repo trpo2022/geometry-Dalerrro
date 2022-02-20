ea90d12b46e3fa9feafbebc17fb1145a9d615dda
Daler Kurbandzhanov <Dalerrro@example.com>
2022-02-20 18:41:50 +0700

    Add all library headers

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+const double Pi = 3.14159265358979323;
+
+int main(){
+    
+}
