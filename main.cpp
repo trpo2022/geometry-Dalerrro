56a74787e75acc24649ae887be252d4ffd3214d6
Daler Kurbandzhanov <Dalerrro@example.com>
2022-02-20 19:03:46 +0700

    Reading file line by line

--- main.cpp
+++ main.cpp
@@ -11,5 +11,17 @@ using namespace std;
 const double Pi = 3.14159265358979323;
 
 int main(){
-    
+    int counter = 0;
+    int storage_size = 100;
+    std::string storage[storage_size];
+    std::string line;
+    std::ifstream in("wkt.wkt");
+    if (in.is_open()) {
+        while (getline(in, line)) {
+            storage[counter] = line;
+            counter++;
+        }
+    } else {
+        printf("Open File Error");
+    }
 }
